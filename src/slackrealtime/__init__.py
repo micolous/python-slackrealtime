38099a48ff187bdb43770035528828faecda287c
Michael Farrell <micolous+git@gmail.com>
2017-09-18 21:57:02 +1000

    Add warning if debug=True is set (fixes issue #3)

--- src/slackrealtime/__init__.py
+++ src/slackrealtime/__init__.py
@@ -1,6 +1,6 @@
 """
 slackrealtime/__init__.py
-Copyright 2014-2015 Michael Farrell <http://micolous.id.au>
+Copyright 2014-2017 Michael Farrell <http://micolous.id.au>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
@@ -18,21 +18,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
 from autobahn.twisted.websocket import WebSocketClientFactory, connectWS
+import warnings
+
 from .protocol import RtmProtocol
 from .session import request_session
 
 def connect(token, protocol=RtmProtocol, factory=WebSocketClientFactory, factory_kwargs=None, api_url=None, debug=False):
 	"""
 	Creates a new connection to the Slack Real-Time API.
-	
+
 	Returns (connection) which represents this connection to the API server.
-	
+
 	"""
 	if factory_kwargs is None:
 		factory_kwargs = dict()
 
 	metadata = request_session(token, api_url)
-	wsfactory = factory(metadata.url, debug=debug, **factory_kwargs)
+	wsfactory = factory(metadata.url, **factory_kwargs)
+	if debug:
+		warnings.warn('debug=True has been deprecated in autobahn 0.14.0')
+
 	wsfactory.protocol = lambda *a,**k: protocol(*a,**k)._seedMetadata(metadata)
 	connection = connectWS(wsfactory)
 	return connection
